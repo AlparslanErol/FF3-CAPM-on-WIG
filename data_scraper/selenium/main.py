13bbf876a4575b10386cccc53e0dd9ef8e80c8c4
AlparslanErol <alparslann.erol@gmail.com>
2022-06-01 17:05:31 +0200

    feat(argparse): Adding arguments into selenium main function.
    Please use python -m data_scraper.selenium.main -s "company" or "wig" based on what you would like to scrape from the source.

--- data_scraper/selenium/main.py
+++ data_scraper/selenium/main.py
@@ -1,10 +1,21 @@
 from data_scraper.selenium.selenium_scraper import SeleniumScraper
+import argparse
 
 
 def main(args=None):
     selenium_scraper = SeleniumScraper()
-    selenium_scraper.company_stock_scraper()
+    selenium_scraper.wig_market_data_scraper() if args.scrape_type == "wig" else selenium_scraper.company_stock_scraper()
 
 
 if __name__ == "__main__":
-    main()
+    my_parser = argparse.ArgumentParser()
+    my_parser.add_argument(
+        "-s",
+        "--scrape-type",
+        type=str,
+        required=False,
+        default="wig",
+        help="Please select scraper type: company or wig",
+    )
+    args = my_parser.parse_args()
+    main(args=args)
